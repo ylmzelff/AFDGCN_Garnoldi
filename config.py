c33e17d7b2d7263d2db916bbcff9d7f9f52eb668
ylmzelff <elifnur.yilmaz@agu.edu.tr>
2025-07-03 21:20:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ GRAPH = "/content/AFDGCN_Garnoldi/data/Konya/konya_kavşaklar.csv"
 #GRAPH = "./data/PEMS04/PEMS04.csv"
 K = 0.1
 ALGO_OPTIONS = ['default', 'Garnoldi', 'APPNP', 'GPRGNN']
-ALGO = ALGO_OPTIONS[2]
+ALGO = ALGO_OPTIONS[3]
 # FILENAME_ID = "./data/PEMS03/PEMS03.txt"
 FILENAME_ID = None
 
