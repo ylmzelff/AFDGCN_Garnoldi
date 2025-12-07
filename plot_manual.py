6215ea244c3449d68e5293878ef26cfb100570c5
ylmzelff <elifnur.yilmaz@agu.edu.tr>
2025-12-07 22:32:46 +0300

    pems04 pred csv konfigurasyonları

--- plot_manual.py
+++ plot_manual.py
@@ -16,9 +16,9 @@ df_pred = pd.read_csv(pred_data_path, skiprows=1, header=None, names=['timestep'
 start_date = pd.Timestamp("2025-02-01 00:00")  # Adjusted to reflect test period
 
 # Time interval details
-time_interval = 10  # Time interval in minutes
+time_interval = 5  # Time interval in minutes
 daily_time_steps = int((24 * 60) / time_interval)  # Number of timesteps per day
-test_time_steps = 3398  # Number of test timesteps (~48.42 hours)
+test_time_steps = 3397  # Number of test timesteps (~48.42 hours)
 
 # Extract relevant test data
 df_pred_location = df_pred[df_pred['location'] == 0].iloc[-test_time_steps:]
@@ -29,8 +29,8 @@ time_steps_pred = [start_date + pd.Timedelta(minutes=time_interval * i) for i in
 time_steps_real = [start_date + pd.Timedelta(minutes=time_interval * i) for i in range(len(df_real_period))]
 
 # Define zoom range based on date and hour
-zoom_start_date = pd.Timestamp("2025-02-01 02:00")  # Modify this
-zoom_end_date = pd.Timestamp("2025-02-01 12:00")    # Modify this
+zoom_start_date = pd.Timestamp("2025-02-08 08:00")  # Day 9 start
+zoom_end_date = pd.Timestamp("2025-02-11 00:00")    # Day 11 start
 
 zoom_start = next(i for i, t in enumerate(time_steps_real) if t >= zoom_start_date)
 zoom_end = next(i for i, t in enumerate(time_steps_real) if t >= zoom_end_date)
