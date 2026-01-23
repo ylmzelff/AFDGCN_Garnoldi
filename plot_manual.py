7104c93d27d092b70e949b114bf572e1f1315854
ylmzelff <elifnur.yilmaz@agu.edu.tr>
2026-01-23 13:27:20 +0300

    kayseri kol bazlı veri konfigurasyonları

--- plot_manual.py
+++ plot_manual.py
@@ -13,12 +13,12 @@ pred_data_path = r"./test_gar.csv"
 df_pred = pd.read_csv(pred_data_path, skiprows=1, header=None, names=['timestep', 'location', 'flow', 'occupy', 'speed'])
 
 # Define Parameters
-start_date = pd.Timestamp("2016-08-30 00:00")  # Last 577 timesteps (2 days prediction)
+start_date = pd.Timestamp("2016-07-30 23:55")  # 8641st timestep (1 month prediction)
 
 # Time interval details
 time_interval = 5  # Time interval in minutes (PEMS08 standard)
 daily_time_steps = int((24 * 60) / time_interval)  # Number of timesteps per day
-test_time_steps = 577  # Number of test timesteps (2 days)
+test_time_steps = 8647  # Number of test timesteps (1 month)
 
 # Extract relevant test data
 df_pred_location = df_pred[df_pred['location'] == 42].iloc[-test_time_steps:]
@@ -29,8 +29,8 @@ time_steps_pred = [start_date + pd.Timedelta(minutes=time_interval * i) for i in
 time_steps_real = [start_date + pd.Timedelta(minutes=time_interval * i) for i in range(len(df_real_period))]
 
 # Define zoom range based on date and hour
-zoom_start_date = pd.Timestamp("2016-08-30 03:00")  # First day morning
-zoom_end_date = pd.Timestamp("2016-08-30 12:00")    # First day evening
+zoom_start_date = pd.Timestamp("2016-08-29 03:00")  # LAST day morning
+zoom_end_date = pd.Timestamp("2016-08-29 12:00")    # LAST day evening
 
 zoom_start = next((i for i, t in enumerate(time_steps_real) if t >= zoom_start_date), 0)
 zoom_end = next((i for i, t in enumerate(time_steps_real) if t >= zoom_end_date), len(time_steps_real) - 1)
